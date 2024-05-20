08a57a19f0d41c57c792fe91f6f2258035c174c2
ChrisFDSTech <146366279+ChrisFDSTech@users.noreply.github.com>
2024-05-20 13:13:57 -0400

    Update PrinterInstall.ps1

--- Printer-Install/PrinterInstall.ps1
+++ Printer-Install/PrinterInstall.ps1
@@ -21,63 +21,63 @@
 
 # Define an array of hashtables with the printer configurations
 $printerConfigs = @(
-    @{ Name = 'Azalea Manor'; IPAddress = '192.168.14.69' },
-    @{ Name = 'Bennettsville Green'; IPAddress = '192.168.5.69' },
-    @{ Name = 'Benson Green'; IPAddress = '192.168.192.69' },
-    @{ Name = 'Blanton Green'; IPAddress = '192.168.41.69' },
-    @{ Name = 'Bordeaux'; IPAddress = '192.168.40.69' },
-    @{ Name = 'Bunce Green'; IPAddress = '192.168.43.69' },
-    @{ Name = 'Bunce Manor'; IPAddress = '192.168.8.69' },
-    @{ Name = 'Cleveland Green III'; IPAddress = '192.168.71.69' },
-    @{ Name = 'Clinton Green'; IPAddress = '192.168.20.69' },
-    @{ Name = 'Club Pond'; IPAddress = '192.168.104.69' },
-    @{ Name = 'Coldwater Ridge'; IPAddress = '192.168.200.69' },
-    @{ Name = 'Cross Creek Pointe'; IPAddress = '192.168.33.69' },
-    @{ Name = 'Crosswinds Green'; IPAddress = '192.168.66.69' },
-    @{ Name = 'Cypress Manor'; IPAddress = '192.168.68.69' },
-    @{ Name = 'Dogwood Manor'; IPAddress = '192.168.10.69' },
-    @{ Name = 'Eastside Green'; IPAddress = '192.168.46.69' },
-    @{ Name = 'Golfview'; IPAddress = '192.168.47.69' },
-    @{ Name = 'Graham Manor'; IPAddress = '192.168.194.69' },
-    @{ Name = 'Haymount Manor'; IPAddress = '192.168.48.69' },
-    @{ Name = 'Hickory Ridge'; IPAddress = '192.168.11.69' },
-    @{ Name = 'Hoke Loop'; IPAddress = '192.168.73.69' },
-    @{ Name = 'Legion Crossing'; IPAddress = '192.168.54.69' },
-    @{ Name = 'Legion Manor'; IPAddress = '192.168.50.69' },
-    @{ Name = 'Longview'; IPAddress = '192.168.51.69' },
-    @{ Name = 'McArthur Park'; IPAddress = '192.168.89.69' },
-    @{ Name = 'Millstone Landing'; IPAddress = '192.168.111.69' },
-    @{ Name = 'Newberry Green'; IPAddress = '192.168.52.69' },
-    @{ Name = 'Oak Run'; IPAddress = '192.168.13.69' },
-    @{ Name = 'Oak Run II'; IPAddress = '192.168.15.69' },
-    @{ Name = 'Palmer Green'; IPAddress = '192.168.53.69' },
-    @{ Name = 'Raeford Green'; IPAddress = '192.168.42.69' },
-    @{ Name = 'Reidsville Ridge'; IPAddress = '192.168.72.69' },
-    @{ Name = 'Riverview Green'; IPAddress = '192.168.44.69' },
-    @{ Name = 'Rosehill West'; IPAddress = '192.168.88.69' },
-    @{ Name = 'Shallotte Villas'; IPAddress = '192.168.58.69' },
-    @{ Name = 'Southview Green'; IPAddress = '192.168.59.69' },
-    @{ Name = 'Southview Townhouses'; IPAddress = '192.168.67.69' },
-    @{ Name = 'Southview Villas'; IPAddress = '192.168.45.69' },
-    @{ Name = 'Spring Lake Green'; IPAddress = '192.168.61.69' },
-    @{ Name = 'Sycamore Park'; IPAddress = '192.168.12.69' },
-    @{ Name = 'Tokay Green'; IPAddress = '192.168.49.69' },
-    @{ Name = 'Wallstreet Green'; IPAddress = '192.168.120.69' },
-    @{ Name = 'Watauga Green'; IPAddress = '192.168.69.69' },
-    @{ Name = 'West Cumberland'; IPAddress = '192.168.92.69' },
-    @{ Name = 'West Fayetteville'; IPAddress = '192.168.204.69' },
-    @{ Name = 'Woodgreen'; IPAddress = '192.168.64.69' },
-    @{ Name = 'Zebulon Green'; IPAddress = '192.168.103.69' }
+    @{ Name = 'Azalea Manor'; IPAddress = '192.168.14.200' },
+    @{ Name = 'Bennettsville Green'; IPAddress = '192.168.5.200' },
+    @{ Name = 'Benson Green'; IPAddress = '192.168.192.200' },
+    @{ Name = 'Blanton Green'; IPAddress = '192.168.41.200' },
+    @{ Name = 'Bordeaux'; IPAddress = '192.168.40.200' },
+    @{ Name = 'Bunce Green'; IPAddress = '192.168.43.200' },
+    @{ Name = 'Bunce Manor'; IPAddress = '192.168.8.200' },
+    @{ Name = 'Cleveland Green III'; IPAddress = '192.168.71.200' },
+    @{ Name = 'Clinton Green'; IPAddress = '192.168.20.200' },
+    @{ Name = 'Club Pond'; IPAddress = '192.168.104.200' },
+    @{ Name = 'Coldwater Ridge'; IPAddress = '192.168.200.200' },
+    @{ Name = 'Cross Creek Pointe'; IPAddress = '192.168.33.200' },
+    @{ Name = 'Crosswinds Green'; IPAddress = '192.168.66.200' },
+    @{ Name = 'Cypress Manor'; IPAddress = '192.168.68.200' },
+    @{ Name = 'Dogwood Manor'; IPAddress = '192.168.10.200' },
+    @{ Name = 'Eastside Green'; IPAddress = '192.168.46.200' },
+    @{ Name = 'Golfview'; IPAddress = '192.168.47.200' },
+    @{ Name = 'Graham Manor'; IPAddress = '192.168.194.200' },
+    @{ Name = 'Haymount Manor'; IPAddress = '192.168.48.200' },
+    @{ Name = 'Hickory Ridge'; IPAddress = '192.168.11.200' },
+    @{ Name = 'Hoke Loop'; IPAddress = '192.168.73.200' },
+    @{ Name = 'Legion Crossing'; IPAddress = '192.168.54.200' },
+    @{ Name = 'Legion Manor'; IPAddress = '192.168.50.200' },
+    @{ Name = 'Longview'; IPAddress = '192.168.51.200' },
+    @{ Name = 'McArthur Park'; IPAddress = '192.168.89.200' },
+    @{ Name = 'Millstone Landing'; IPAddress = '192.168.111.200' },
+    @{ Name = 'Newberry Green'; IPAddress = '192.168.52.200' },
+    @{ Name = 'Oak Run'; IPAddress = '192.168.13.200' },
+    @{ Name = 'Oak Run II'; IPAddress = '192.168.15.200' },
+    @{ Name = 'Palmer Green'; IPAddress = '192.168.53.200' },
+    @{ Name = 'Raeford Green'; IPAddress = '192.168.42.200' },
+    @{ Name = 'Reidsville Ridge'; IPAddress = '192.168.72.200' },
+    @{ Name = 'Riverview Green'; IPAddress = '192.168.44.200' },
+    @{ Name = 'Rosehill West'; IPAddress = '192.168.88.200' },
+    @{ Name = 'Shallotte Villas'; IPAddress = '192.168.58.200' },
+    @{ Name = 'Southview Green'; IPAddress = '192.168.59.200' },
+    @{ Name = 'Southview Townhouses'; IPAddress = '192.168.67.200' },
+    @{ Name = 'Southview Villas'; IPAddress = '192.168.45.200' },
+    @{ Name = 'Spring Lake Green'; IPAddress = '192.168.61.200' },
+    @{ Name = 'Sycamore Park'; IPAddress = '192.168.12.200' },
+    @{ Name = 'Tokay Green'; IPAddress = '192.168.49.200' },
+    @{ Name = 'Wallstreet Green'; IPAddress = '192.168.120.200' },
+    @{ Name = 'Watauga Green'; IPAddress = '192.168.69.200' },
+    @{ Name = 'West Cumberland'; IPAddress = '192.168.92.200' },
+    @{ Name = 'West Fayetteville'; IPAddress = '192.168.204.200' },
+    @{ Name = 'Woodgreen'; IPAddress = '192.168.64.200' },
+    @{ Name = 'Zebulon Green'; IPAddress = '192.168.103.200' }
 )
 
 # Define the GitHub URLs for the files
-$msiUrl = "https://github.com/ChrisFDSTech/Scripts/blob/main/Printer-Install/6900.msi"
+$msiUrl = "https://github.com/ChrisFDSTech/Scripts/blob/main/Printer-Install/20000.msi"
 $modelDatUrl = "https://github.com/ChrisFDSTech/Scripts/blob/main/Printer-Install/model023.dat"
 
 # Define the directory and temp paths
 $directoryPath = "C:\Program Files\FDS"
 $tempDirectoryPath = "$directoryPath\temp"
-$tempMsiPath = [System.IO.Path]::Combine($tempDirectoryPath, "6900.msi")
+$tempMsiPath = [System.IO.Path]::Combine($tempDirectoryPath, "20000.msi")
 $tempModelDatPath = [System.IO.Path]::Combine($tempDirectoryPath, "model023.dat")
 
 # Ensure the temp directory exists
@@ -92,7 +92,7 @@ Invoke-WebRequest -Uri $msiUrl -OutFile $tempMsiPath
 Invoke-WebRequest -Uri $modelDatUrl -OutFile $tempModelDatPath
 
 # Define a template for the command
-$commandTemplate = 'msiexec /i "{0}" /q DRIVERNAME="Brother MFC-L6900DW series" PRINTERNAME="{1}" ISDEFAULTPRINTER="0" IPADDRESS="{2}"'
+$commandTemplate = 'msiexec /i "{0}" /q DRIVERNAME="Brother MFC-L20000DW series" PRINTERNAME="{1}" ISDEFAULTPRINTER="0" IPADDRESS="{2}"'
 
 # Get the current IP address of the machine
 $CurrentIPAddress = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -notlike '*Loopback*' }).IPAddress
