90975f3eb8ad7168634c7105332fd43275c4fd6d
ChrisFDSTech <146366279+ChrisFDSTech@users.noreply.github.com>
2024-05-21 15:44:58 -0400

    Update Uninstall-PrinterInstall.ps1

--- Printer-Install/Uninstall-PrinterInstall.ps1
+++ Printer-Install/Uninstall-PrinterInstall.ps1
@@ -1,18 +1,65 @@
-# Define the directory path
-$directoryPath = "C:\ProgramData\FDS"
-$printersFilePath = Join-Path $directoryPath "Printers.txt"
+# Define the list of printer names to check for
+$printerNames = @(
+    'Azalea Manor',
+    'Bennettsville Green',
+    'Benson Green',
+    'Blanton Green',
+    'Bordeaux',
+    'Bunce Green',
+    'Bunce Manor',
+    'Cleveland Green III',
+    'Clinton Green',
+    'Club Pond',
+    'Coldwater Ridge',
+    'Cross Creek Pointe',
+    'Crosswinds Green',
+    'Cypress Manor',
+    'Dogwood Manor',
+    'Eastside Green',
+    'Golfview',
+    'Graham Manor',
+    'Haymount Manor',
+    'Hickory Ridge',
+    'Hoke Loop',
+    'Legion Crossing',
+    'Legion Manor',
+    'Longview',
+    'McArthur Park',
+    'Millstone Landing',
+    'Newberry Green',
+    'Oak Run',
+    'Oak Run II',
+    'Palmer Green',
+    'Raeford Green',
+    'Reidsville Ridge',
+    'Riverview Green',
+    'Rosehill West',
+    'Shallotte Villas',
+    'Southview Green',
+    'Southview Townhouses',
+    'Southview Villas',
+    'Spring Lake Green',
+    'Sycamore Park',
+    'Tokay Green',
+    'Wallstreet Green',
+    'Watauga Green',
+    'West Cumberland',
+    'West Fayetteville',
+    'Woodgreen',
+    'Zebulon Green',
+    'Intune Printer'
+)
 
-# Check if the Printers.txt file exists
-if (Test-Path $printersFilePath) {
-    # Read the printer names from the file
-    $printerNames = Get-Content $printersFilePath
+# Get a list of all installed printers
+$installedPrinters = Get-Printer -Full
 
-    # Loop through each printer name and uninstall the printer
-    foreach ($printerName in $printerNames) {
+# Loop through each printer name in the list
+foreach ($printerName in $printerNames) {
+    # Check if the printer is installed
+    $printer = $installedPrinters | Where-Object { $_.Name -eq $printerName }
+
+    if ($printer) {
         try {
-            # Get the printer object
-            $printer = Get-Printer -Name $printerName -ErrorAction Stop
-
             # Uninstall the printer
             $null = $printer.RemovePrinter()
             Write-Host "Printer '$printerName' has been uninstalled."
@@ -21,7 +68,7 @@ if (Test-Path $printersFilePath) {
             Write-Warning "Failed to uninstall printer '$printerName': $_"
         }
     }
-}
-else {
-    Write-Warning "The Printers.txt file does not exist in $directoryPath"
+    else {
+        Write-Host "Printer '$printerName' is not installed."
+    }
 }
