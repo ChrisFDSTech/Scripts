0f1bf9e9f51da4e238855f9d1a5f5f983aecb136
ChrisFDSTech <146366279+ChrisFDSTech@users.noreply.github.com>
2024-05-23 15:44:52 -0400

    Update PrinterInstall.ps1

--- Printer-Install/PrinterInstall.ps1
+++ Printer-Install/PrinterInstall.ps1
@@ -232,6 +232,10 @@ catch {
                 $message = "The $($config.Name) printer was installed."
                 Write-Host $message
                 Show-PopupMessageWithImage $message "Printer Installed" $tempImagePath
+
+                # Create a text file with the printer name
+                $printerNameFile = Join-Path $tempDirectoryPath "$($config.Name).txt"
+                Set-Content -Path $printerNameFile -Value $config.Name
             }
             $matched = $true
             break
